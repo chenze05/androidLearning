94ac3d4b7448d943cc79525d7827c1dde8bb3f6b
chenze <chenze05@qq.com>
2018-06-22 19:37:31 +0800

    learning..

--- app/src/main/java/org/chenze/androidlearning/MainActivity.java
+++ app/src/main/java/org/chenze/androidlearning/MainActivity.java
@@ -2,12 +2,25 @@ package org.chenze.androidlearning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private EditText editText;
+
+    public MainActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        textView.setText("Hello, I am chenze.");
+
+        editText = findViewById(R.id.editText);
+
     }
 }
