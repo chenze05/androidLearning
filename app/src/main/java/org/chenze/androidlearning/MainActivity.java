efdcf6f6f0c46bdf12c0d08737e646b02759301f
chenze <chenze05@qq.com>
2018-06-29 11:59:54 +0800

    add menu

--- app/src/main/java/org/chenze/androidlearning/MainActivity.java
+++ app/src/main/java/org/chenze/androidlearning/MainActivity.java
@@ -2,6 +2,8 @@ package org.chenze.androidlearning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -20,5 +22,25 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "you clicked the button.", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId())
+        {
+            case R.id.add_item:
+                Toast.makeText(this, "you selected add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "you selected remove", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
